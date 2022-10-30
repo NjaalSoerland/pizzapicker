9e34918665afb1684ee34b676fe8feb61a267c52
Njål Sørland <njaalis2000@gmail.com>
2022-10-30 23:49:03 +0100

    created api for pizza fetching

---
+++ src/app.py
@@ -0,0 +1,30 @@
+from requests_html import AsyncHTMLSession
+from flask import Flask
+import nest_asyncio
+nest_asyncio.apply()
+
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+async def fetchPizzas(URL = "https://www.pizzabakeren.no/originalpizza"):
+    htmlSession = AsyncHTMLSession()
+    r = await htmlSession.get(URL)
+
+    r.html.arender()
+
+    pizzas = {"original": [], "tynn": [], "vegansk": [], "dessert": []}
+
+    links = r.html.absolute_links
+    for link in links:
+        for category in pizzas.keys():
+            if category in link:
+                session = await htmlSession.get(link)
+                session.html.arender()
+                
+                options = session.html.find(".content")
+                for option in options:
+                    pizzas[category].append(option.text)
+
+    return pizzas
+
+if __name__ == '__main__':
+    app.run(debug=True)
